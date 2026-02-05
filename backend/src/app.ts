efa3b4251a2b4b7ae224413e1ec36b88945212eb
Aleksandr Drozdov <d-a-s91@yandex.ru>
2026-02-05 18:01:17 +0300

    test

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -5,26 +5,50 @@ import 'dotenv/config'
 import express, { json, urlencoded } from 'express'
 import mongoose from 'mongoose'
 import path from 'path'
+import helmet from 'helmet'
+import  csrf from 'csurf'
 import { DB_ADDRESS } from './config'
 import errorHandler from './middlewares/error-handler'
 import serveStatic from './middlewares/serverStatic'
 import routes from './routes'
 
+
 const { PORT = 3000 } = process.env
 const app = express()
 
+app.use(helmet())
+
 app.use(cookieParser())
 
-app.use(cors())
-// app.use(cors({ origin: ORIGIN_ALLOW, credentials: true }));
+app.use(cors({ origin: true, credentials: true }));
 // app.use(express.static(path.join(__dirname, 'public')));
-
-app.use(serveStatic(path.join(__dirname, 'public')))
 
 app.use(urlencoded({ extended: true }))
 app.use(json())
 
-app.options('*', cors())
+app.use((req, res, next) => {
+    if (['GET', 'HEAD', 'OPTIONS'].includes(req.method)) {
+        return next()
+    }
+
+    return csrf({
+        cookie: {
+            httpOnly: true,
+            sameSite: 'strict',
+        },
+    })(req, res, next)
+})
+
+app.use((req, res, next) => {
+    if (req.csrfToken) {
+        res.cookie('XSRF-TOKEN', req.csrfToken())
+    }
+    next()
+})
+
+app.use(serveStatic(path.join(__dirname, 'public')))
+
+
 app.use(routes)
 app.use(errors())
 app.use(errorHandler)
